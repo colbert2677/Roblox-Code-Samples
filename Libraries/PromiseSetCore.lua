13bcb27b38d633d8c3cdd72aa7a72033a9a6d780
Allen S. Crow <16709300+colbert2677@users.noreply.github.com>
2020-09-16 01:52:02 -0400

    Improve PromiseSetCore implementation
    Deleted a lot of excess function bulk and simplified the implementation by learning that a single function does not need to be one Promise, rather it's more imperative that one is returned when it comes to chaining.

--- Libraries/PromiseSetCore.lua
+++ Libraries/PromiseSetCore.lua
@@ -1,34 +1,27 @@
 --- Wrap SetCore attempts in a retrying Promise
+-- Enforces that SetCore successfully processes request.
 -- @module PromiseSetCore
+-- @author colbert2677
 
 local StarterGui = game:GetService("StarterGui")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Promise = require("Promise")
 
-local MAX_RETRIES = 15
-
 --- Make Promise to use SetCore in
--- @see promiseRetry
-local function setCore(coreType, value)
-    return Promise.new(function (resolve)
-        StarterGui:SetCore(coreType, value)
-        
-        resolve()
-    end)
-end
-
---- Handle unsuccessful Promise resolution
-local function promiseRetryRejected()
-    warn("PromiseSetCore could not be resolved")
-end
-
---- Retry the Promise from SetCore
 -- @param coreType Core type to call SetCore on
 -- @param value Expected future value of core item
 -- @return Promise The retried Promise
-local function promiseRetry(coreType, value)
-    return Promise.retry(setCore, MAX_RETRIES, coreType, value):catch(promiseRetryRejected)
+local function setCore(coreType, value)
+	local setCorePromise = Promise.new(function (resolve)
+		StarterGui:SetCore(coreType, value)
+		
+		resolve(StarterGui:GetCore(coreType))
+    end)
+    
+    return Promise.retry(setCorePromise, math.huge, coreType, value):catch(function ()
+        warn("PromiseSetCore unexpectedly could not set " .. coreType)
+    end)
 end
 
-return promiseRetry
+return setCore
