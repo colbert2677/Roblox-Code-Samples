92b482c570ca5088a7ccfa82300734841ca6a6b8
Allen S. Crow <16709300+colbert2677@users.noreply.github.com>
2020-09-16 02:30:32 -0400

    Improve PromiseSetCore implementation (2)
    Revised the earlier improvements with discussion and help from evaera (author of Promise library). This should be the final iteration for some time (or for good), as well as the final unbranched feature. Something frequently changing like this shouldn't be committed to the main branch frequently.

--- Libraries/PromiseSetCore.lua
+++ Libraries/PromiseSetCore.lua
@@ -5,9 +5,9 @@
 local StarterGui = game:GetService("StarterGui")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Promise = require(ReplicatedStorage.Scripts.Modules.Promise)
+local Promise = require("Promise")
 
-local MAX_RETRIES = 15
+local MAX_RETRIES = 10
 
 --- Make Promise to use SetCore in
 -- @see promiseRetry
@@ -16,17 +16,21 @@ local function setCore(coreType, value)
 		StarterGui:SetCore(coreType, value)
 		
 		resolve()
-	end)
+    end):catch(function (e)
+        return Promise.delay(1):andThen(function ()
+            return Promise.reject(e)
+        end)
+    end)
 end
 
 --- Retry the Promise from SetCore
 -- @param coreType Core type to call SetCore on
 -- @param value Expected future value of core item
 -- @return Promise The retried Promise
-local function promiseRetry(coreType, value)
+local function PromiseSetCore(coreType, value)
 	return Promise.retry(setCore, MAX_RETRIES, coreType, value):catch(function ()
 		warn("PromiseSetCore could not be resolved")
 	end)
 end
 
-return promiseRetry
+return PromiseSetCore
