36a3f86a06489e1d9108cefe0a4b7de7d0e122b6
Pnlmon <Pnlmon@please-end-your-fucking.life>
2020-09-27 17:13:57 +0800

    Shorter code
    Use a "or" instead of two if statement (Read line 12)
    Only use one return instead of two (Read line 21)

--- Samples/SwallowShadowBanChats.lua
+++ Samples/SwallowShadowBanChats.lua
@@ -9,20 +9,16 @@ local function Run(ChatService)
 		local channel = ChatService:GetChannel(channelName)
 		
 		-- If neither exists, we're safe to continue processing via other methods
-		if not speaker then return false end
-		if not channel then return false end
+		if not speaker or not channel then return false end
 		
 		-- Check if they are shadow banned
 		if speaker.ShadowBanned then
 			-- Send the message to themselves
 			speaker:SendMessage(message, channelName, speakerName, message.ExtraData)
-			
-			-- Speaker was shadow banned, stop processing message
-			return true
 		end
 		
-		-- Speaker is fine, continue processing message
-		return false
+		-- Return the speaker's shadows banned status
+		return speaker.ShadowBanned
 	end
 	
 	ChatService:RegisterProcessCommandsFunction("swallow_shadow_ban_chat", ProcessMessage)
